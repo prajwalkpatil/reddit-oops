02bc251701a6ae84e15330e399b1e4ce0c1fa5c9
Prajwal Patil <prajwalpatilk@gmail.com>
2022-05-23 12:02:25 +0530

    Add factory design pattern

--- main.cpp
+++ main.cpp
@@ -17,7 +17,15 @@ int main()
         cout << "Error!" << endl;
         exit(-1);
     }
+    getchar();
+    Comment *c = new Comment("Hello everyone", "freakinginsane", "2003 8:47");
+    Subreddit *s = new Subreddit("askreddit");
+    u.createPost(s);
+    s->printPosts();
+    // u.replyIt(c);
+    cout << "**********************************" << endl;
 
-    u.printDetails();
+    c->replies[0]->printComment();
+    // u.printDetails();
     return 0;
 }
